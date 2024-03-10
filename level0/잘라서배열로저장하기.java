537d1d3eaf0d4aff717e628747abe5a5dde1d8f3
gomyeongbo <kmyeongb@gmail.com>
2024-03-10 22:16:49 +0900

    Day 10

---
+++ level0/잘라서배열로저장하기.java
@@ -0,0 +1,21 @@
+public class 잘라서배열로저장하기 {
+    public String[] solution(String my_str, int n) {
+        int length = 0;
+        if (my_str.length() % n == 0) {
+            length = my_str.length() / n;
+        } else {
+            length = my_str.length() / n + 1;
+        }
+
+        String[] answer = new String[length];
+        for (int i = 0; i < length; i++) {
+            if (my_str.length() >= n) {
+                answer[i] = my_str.substring(0, n);
+                my_str = my_str.substring(n, my_str.length());
+            } else {
+                answer[i] = my_str.substring(0, my_str.length());
+            }
+        }
+        return answer;
+    }
+}
